708809480ebc4b0d6e39b6622388f83d6db4737e
Marko Lyly <Marko.Lyly@proactum.fi>
2015-03-20 14:12:46 +0200

    Added simple rest controller for items

---
+++ src/main/java/fi/zcode/jenkins/rest/ItemController.java
@@ -0,0 +1,37 @@
+package fi.zcode.jenkins.rest;
+
+import fi.zcode.jenkins.domain.Item;
+import fi.zcode.jenkins.repository.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/items")
+public class ItemController {
+    @Autowired
+    private ItemRepository repo;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List findItems() {
+        return repo.findAll();
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public Item addItem(@RequestBody Item item) {
+        item.setId(null);
+        return repo.saveAndFlush(item);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public Item updateItem(@RequestBody Item updatedItem, @PathVariable Integer id) {
+        updatedItem.setId(id);
+        return repo.saveAndFlush(updatedItem);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public void deleteItem(@PathVariable Integer id) {
+        repo.delete(id);
+    }
+}
